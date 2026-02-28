536f3ffef02e00647e14c28f2efb7b93849f1a15
9adithyakrishna-coder <9adithyakrishna@gmail.com>
2026-02-28 19:20:50 +0100

    Add files via upload

---
+++ Lesson7.py
@@ -0,0 +1,8 @@
+#Taking input from the user
+word = input("Enter the word: ")
+
+#Converting to uppercase
+uppercase_word = word.upper()
+
+#Printing the result
+print("The word in uppercase is:", uppercase_word)
