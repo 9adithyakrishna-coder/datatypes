0fe503c0472ddca1914021a0d04407f8b8f776ae
9adithyakrishna-coder <9adithyakrishna@gmail.com>
2026-03-02 17:15:43 +0100

    Add files via upload

---
+++ Lesson10.py
@@ -0,0 +1,8 @@
+actual_cost = float(input("please enter the actual product price: "))
+sale_amount = float(input("please enter the actual product price: "))
+
+if (sale_amount > actual_cost):  
+    amount = sale_amount - actual_cost
+    print("total profit",amount)
+else:
+    print("no profit!!!")       
