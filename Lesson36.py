4446cc24b45586f6ff3e81a407ed2545aec2b39b
9adithyakrishna-coder <9adithyakrishna@gmail.com>
2026-05-03 18:56:56 +0200

    Add files via upload

---
+++ Lesson36.py
@@ -0,0 +1,11 @@
+def decimal_to_binary(n):
+    if n == 0:
+        return "0"
+    if n == 1:
+        return "1"
+    
+    return decimal_to_binary(n // 2) + str(n % 2)
+
+# Example usage
+number = int(input("Enter a decimal number: "))
+print("Binary representation:", decimal_to_binary(number))
