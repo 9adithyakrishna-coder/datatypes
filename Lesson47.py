bb96de65909d7d45df97e5168b7aecfd0bb52c21
9adithyakrishna-coder <9adithyakrishna@gmail.com>
2026-05-20 17:15:20 +0200

    Add files via upload

---
+++ Lesson47.py
@@ -0,0 +1,11 @@
+#Take user input
+a = input ("Enter a word: ")
+#program to check break keyword
+for i in a: #iterate for loop
+    if (i == 'A'): #condition 1
+    #display results
+      print ("A is found")
+      break #break statement
+    else:
+    #display results
+      print ("A not found")
