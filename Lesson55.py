149dada961a5c3a49f383c80db3b64e0c326763e
9adithyakrishna-coder <9adithyakrishna@gmail.com>
2026-05-27 17:19:26 +0200

    Add files via upload

---
+++ Lesson55.py
@@ -0,0 +1,16 @@
+import random #importing module
+playing = True #initialise
+number = str(random.randint(0,9)) #random in-built function
+
+print("i will generate a number from 0 to 9, and you have to guess the number one digit at a time.")
+print("The game ends when you get 1 hero!")
+#iterate loop till the condition is true
+while playing:
+  guess = input("Give me your best guess! \n")
+  if number == guess:
+    print("you win the game")
+    print("the number was",number)
+    break
+  
+  else:
+    print("your guess isnt quite right, try again. \n")
