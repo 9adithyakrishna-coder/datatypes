063595fa59016c00ec430939b18f40776b79d85f
9adithyakrishna-coder <9adithyakrishna@gmail.com>
2026-07-01 17:46:52 +0200

    Add files via upload

---
+++ Lesson77.py
@@ -0,0 +1,16 @@
+rajesh_score = ["name", "rajesh", "score", "97"]
+nayana_score = ["name", "nayana",  "score", "68"]
+suresh_score = ["name", "suresh", "score", "89"]
+ganesh_score = ["name", "ganesh", "score", "57"]
+
+all_scores =(rajesh_score, nayana_score, suresh_score, ganesh_score)
+
+max_score = (rajesh_score)
+min_score = (ganesh_score)
+average_score = (suresh_score)
+ 
+class_ranking =[rajesh_score, suresh_score, nayana_score, ganesh_score,]
+rajesh_score
+
+print(all_scores)
+
