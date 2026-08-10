d2a11fc569bd25859554a7c2bcbd2cad642f2587
9adithyakrishna-coder <9adithyakrishna@gmail.com>
2026-08-10 17:44:43 +0200

    Add files via upload

---
+++ Lesson95.py
@@ -0,0 +1,13 @@
+class library:
+    def __init__(self, Books):
+        self.books = Books
+
+class Book:
+    def __init__(self, title, author, is_borrowed):
+        self.title = title
+        self.author = author
+        self.is_borrowed = is_borrowed
+
+title =(robin hood):
+
+print(title)
